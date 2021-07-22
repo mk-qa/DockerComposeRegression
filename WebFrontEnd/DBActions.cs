d88115866815170f9fe18d5a05a2eb2312374c22
mk <marianna.kurtukova@jetbrains.com>
2021-07-22 14:55:11 +0300

    Initial commit

---
+++ WebFrontEnd/DBActions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Npgsql;
+
+namespace WebFrontEnd
+{
+    public static class DbActions
+    {
+        
+        public static string GetDbVersion()
+        {
+            var cs = $"Host=db;Username=postgres;Password=postgres;Database=postgres";
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+            var cmd = new NpgsqlCommand("SELECT version()", con);
+            return cmd.ExecuteScalar()?.ToString();
+        }
+        
+    }
+}
---
+++ WebFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Hello!";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://mywebapi/WeatherForecast");
+                var response = await client.SendAsync(request);
+
+                ViewData["Message"] += "This is data from API: " + await response.Content.ReadAsStringAsync();
+               
+            }
+        }
+        
+    }
+}
